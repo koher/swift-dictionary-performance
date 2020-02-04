98fc27ef7ebc4bce15185dfae775d08cb977ca83
Yuta Koshizawa <koher@koherent.org>
2020-02-04 19:35:57 +0900

    Replace test with ones for `[Int: Set<Int>]`

--- Tests/SwiftDictionaryPerformanceTests/SwiftDictionaryPerformanceTests.swift
+++ Tests/SwiftDictionaryPerformanceTests/SwiftDictionaryPerformanceTests.swift
@@ -5,105 +5,113 @@ private let n = 100_000 + (0..<2).randomElement()!
 
 final class SwiftDictionaryPerformanceTests: XCTestCase {
     func testDictionary_1() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<1).map { ($0, $0 + 1) })
-        XCTAssertEqual(dictionary.count, 1)
-        let count = dictionary.count
+        let count = 1
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
+        XCTAssertEqual(dictionary.count, 100)
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
     
     func testDictionary_10() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<10).map { ($0, $0 + 1) })
-        XCTAssertEqual(dictionary.count, 10)
-        let count = dictionary.count
+        let count = 10
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
+        XCTAssertEqual(dictionary.count, 100)
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
     
     func testDictionary_100() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<100).map { ($0, $0 + 1) })
+        let count = 100
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
         XCTAssertEqual(dictionary.count, 100)
-        let count = dictionary.count
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
     
     func testDictionary_1_000() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<1_000).map { ($0, $0 + 1) })
-        XCTAssertEqual(dictionary.count, 1_000)
-        let count = dictionary.count
+        let count = 1_000
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
+        XCTAssertEqual(dictionary.count, 100)
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
     
     func testDictionary_10_000() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<10_000).map { ($0, $0 + 1) })
-        XCTAssertEqual(dictionary.count, 10_000)
-        let count = dictionary.count
+        let count = 10_000
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
+        XCTAssertEqual(dictionary.count, 100)
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
     
     func testDictionary_100_000() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<100_000).map { ($0, $0 + 1) })
-        XCTAssertEqual(dictionary.count, 100_000)
-        let count = dictionary.count
+        let count = 100_000
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
+        XCTAssertEqual(dictionary.count, 100)
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
-    
+
     func testDictionary_1_000_000() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<1_000_000).map { ($0, $0 + 1) })
-        XCTAssertEqual(dictionary.count, 1_000_000)
-        let count = dictionary.count
+        let count = 1_000_000
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
+        XCTAssertEqual(dictionary.count, 100)
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
-    
+
     func testDictionary_10_000_000() {
-        var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<10_000_000).map { ($0, $0 + 1) })
-        XCTAssertEqual(dictionary.count, 10_000_000)
-        let count = dictionary.count
+        let count = 10_000_000
+        var dictionary: [Int: Set<Int>] = .init(uniqueKeysWithValues: (0..<100).map { ($0, Set(0..<count)) })
+        XCTAssertEqual(dictionary.count, 100)
+        XCTAssertEqual(dictionary[0]!.count, count)
         measure {
             for i in 0..<n {
-                let key = i % count
-                dictionary[key] = i + 2
-                XCTAssertEqual(dictionary[key], i + 2)
+                let key = i % 100
+                dictionary[key]?.insert(i)
+                XCTAssertTrue(dictionary[key]!.contains(i))
             }
         }
     }
