e59e50992fccd5b7d77b1e22616dc129ad08fea8
Yuta Koshizawa <koher@koherent.org>
2020-02-04 18:04:05 +0900

    Test counts of dictionaries

--- Tests/SwiftDictionaryPerformanceTests/SwiftDictionaryPerformanceTests.swift
+++ Tests/SwiftDictionaryPerformanceTests/SwiftDictionaryPerformanceTests.swift
@@ -6,6 +6,7 @@ private let n = 100_000 + (0..<2).randomElement()!
 final class SwiftDictionaryPerformanceTests: XCTestCase {
     func testDictionary_1() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<1).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 1)
         let count = dictionary.count
         measure {
             for i in 0..<n {
@@ -18,6 +19,7 @@ final class SwiftDictionaryPerformanceTests: XCTestCase {
     
     func testDictionary_10() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<10).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 10)
         let count = dictionary.count
         measure {
             for i in 0..<n {
@@ -30,6 +32,7 @@ final class SwiftDictionaryPerformanceTests: XCTestCase {
     
     func testDictionary_100() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<100).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 100)
         let count = dictionary.count
         measure {
             for i in 0..<n {
@@ -42,6 +45,7 @@ final class SwiftDictionaryPerformanceTests: XCTestCase {
     
     func testDictionary_1_000() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<1_000).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 1_000)
         let count = dictionary.count
         measure {
             for i in 0..<n {
@@ -54,6 +58,7 @@ final class SwiftDictionaryPerformanceTests: XCTestCase {
     
     func testDictionary_10_000() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<10_000).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 10_000)
         let count = dictionary.count
         measure {
             for i in 0..<n {
@@ -66,6 +71,7 @@ final class SwiftDictionaryPerformanceTests: XCTestCase {
     
     func testDictionary_100_000() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<100_000).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 100_000)
         let count = dictionary.count
         measure {
             for i in 0..<n {
@@ -78,6 +84,7 @@ final class SwiftDictionaryPerformanceTests: XCTestCase {
     
     func testDictionary_1_000_000() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<1_000_000).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 1_000_000)
         let count = dictionary.count
         measure {
             for i in 0..<n {
@@ -90,6 +97,7 @@ final class SwiftDictionaryPerformanceTests: XCTestCase {
     
     func testDictionary_10_000_000() {
         var dictionary: [Int: Int] = .init(uniqueKeysWithValues: (0..<10_000_000).map { ($0, $0 + 1) })
+        XCTAssertEqual(dictionary.count, 10_000_000)
         let count = dictionary.count
         measure {
             for i in 0..<n {
